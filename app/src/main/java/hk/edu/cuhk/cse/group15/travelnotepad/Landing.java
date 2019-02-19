f269b2f9f93f9a7148c9d35e36b129481f5178d2
Thomaslkf <1155085844@link.cuhk.edu.hk>
2019-02-19 15:47:38 +0800

    Initial commit

---
+++ app/src/main/java/hk/edu/cuhk/cse/group15/travelnotepad/Landing.java
@@ -0,0 +1,14 @@
+package hk.edu.cuhk.cse.group15.travelnotepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing);
+
+    }
+}
