0461be56b5137e75cf9b77106efe33461cf27a3d
Thomaslkf <1155085844@link.cuhk.edu.hk>
2019-04-24 17:25:02 +0800

    doing the UI part for creating new trip information

---
+++ app/src/main/java/hk/edu/cuhk/cse/group15/travelnotepad/TripCreation.java
@@ -0,0 +1,13 @@
+package hk.edu.cuhk.cse.group15.travelnotepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trip_creation);
+    }
+}
